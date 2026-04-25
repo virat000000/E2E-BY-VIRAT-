086a5bd464e5832fbfa96b5c431d6fdf42c6037a
virat000000 <convofyter@gmail.com>
2026-04-25 11:40:30 +0530

    Delete gunicorn_config.py

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-# gunicorn_config.py
-bind = "0.0.0.0:%s" % os.environ.get("PORT", 8000)
-workers = 4  # Multiple threads chalane ke liye
-timeout = 30
-accesslog = "-"
-errorlog = "-"
-capture_output = True
-enable_stdio_inheritance = True
